5e1d29261ed63948166089b0bd33d9a7a9a626b5
Davidsoncole <davidson4ice@yahoo.com>
2020-06-09 12:01:02 +0100

    corrected main file

--- src/main.js
+++ src/main.js
@@ -2,15 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
-import { store } from './store/store'
-import axios from 'axios';
 
 Vue.config.productionTip = false
 
 axios.defaults.baseURL= '/';
 
 new Vue({
-  store: store,
   router,
   vuetify,
   render: h => h(App)
